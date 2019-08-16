cb597b33458de07901a1427da97de30bb5adeb4a
Haley Ressl <haleyressl@gmail.com>
2019-08-16 10:43:39 -0500

    adding a bunch of stuff because apparently my mac is recalled yippee

--- src/js/site.js
+++ src/js/site.js
@@ -26,6 +26,22 @@ $('input[type="radio"]').click(function(){
 });
 
 
+// scroll to top arrow
+$(document).ready(function(){ 
+    $(window).scroll(function(){ 
+        if ($(this).scrollTop() > 100) { 
+            $('#scroll').fadeIn('slow'); 
+        } else { 
+            $('#scroll').fadeOut('slow'); 
+        } 
+    }); 
+    $('#scroll').click(function(){ 
+        $("html, body").animate({ scrollTop: 0 }, 600); 
+        return false; 
+    }); 
+});
+
+
 
 
 
